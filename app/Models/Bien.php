9a85f8344f29eff8a0fa8351bbf5121c3b47421d
artefarte001 <cheikhsane656@gmail.com>
2024-06-04 14:14:20 +0000

    cheikh

--- app/Models/Bien.php
+++ app/Models/Bien.php
@@ -8,4 +8,16 @@ use Illuminate\Database\Eloquent\Model;
 class Bien extends Model
 {
     use HasFactory;
+
+    protected $fillable = ['name', 'categorie', 'image', 'description', 'adresse', 'statut', 'date_ajout'];
+
+    public function __construct(array $attributes = []) // Constructeur pour plus assignement
+    {
+        parent::__construct($attributes);
+    }
+
+    public function save(array $options = []) // Override default save method (optional)
+    {
+        parent::save($options);
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -4,8 +4,16 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\BienController;
 
 Route::get('/', [BienController::class, 'index']);
-Route::get('/bien', [bienController::class, 'ListeBien']);
+Route::get('/bien', [BienController::class, 'ListeBien']);
 
 Route::get('bien/ajouter', [BienController::class, 'AjouterBien']);
 Route::post('/ajouter/bien-traitement', [BienController::class, 'AjouterBienTraitement']);
 Route::get('/detail-bien/{id}', [BienController::class, 'detail_bien']);
+
+
+// route pour la modification de biens
+Route::post('/modifier/bien-traitement/', [BienController::class, 'ModifierBienTraitement']);
+Route::get('/modifier-bien/{id}', [BienController::class, 'ModifierBien']);
+
+// Route::post('traitement' ,[BienController::class , 'ModifierTraitement']);
+
